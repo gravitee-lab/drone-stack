876236d18fd350f840603afb8f3b423eda527314
Jean-Baptiste-Lasselle <jean.baptiste.lasselle.pegasus@gmail.com>
2020-08-04 21:03:38 +0200

    feat(simple_compose): adding other drone service in docker-compose.yml, to configure from github, [https://github.com/gravitee-lab/drone-stack/issues/1] + [https://github.com/gravitee-lab/drone-stack/issues/4]

---
+++ documentation/webhooks/README.md
@@ -0,0 +1,6 @@
+# Drone CI integration with Git Service Provider
+
+This integration is based on webhooks, which require TLS communication.
+
+Therefore :
+* The `git.pegasusio.io`
--- terraform/apply.sh
+++ terraform/apply.sh
@@ -1,4 +1,25 @@
 #!/bin/bash
 
 # -
-echo "implementation not finished"
+echo " Implementation not finished"
+echo " Use : "
+echo "https://github.com/pegasus-io/a-k8s-demo/releases/tag/0.0.4"
+echo " To provision the EC2 instance"
+
+
+
+# To set up your local work environment, execute this in an empty directory :
+
+# You choose your method, SSH or HTTPS
+export SSH_URI_TO_THIS_REPO=git@github.com:pegasus-io/a-k8s-demo.git
+export URI_DE_CE_REPO=https://github.com/pegasus-io/a-k8s-demo.git
+export THIS_RECIPES_RELEASE=0.0.4
+
+git clone "$URI_DE_CE_REPO" .
+git checkout $THIS_RECIPES_RELEASE
+chmod +x ./load.pipeline.sh
+./load.pipeline.sh
+
+# To terraform the k8s cluster, execute, in the same shell session, and the same directory, the command :
+
+./run.pipeline.sh
